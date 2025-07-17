02529f13429af9118ac4dc2464868217b25a0478
MattiP99 <pirasmattia2299@gmail.com>
2025-07-17 14:54:59 +0200

    changes to API generation and connection with authorizer

--- js/config.js
+++ js/config.js
@@ -5,10 +5,8 @@ window._config = {
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qddb5sbcpd.execute-api.eu-north-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
 
-/*
-eyJraWQiOiJGTExMa2p1NStSTlNIQm0xYjE2SWp3UHFVRnNDd2xoRzErUmFOSVFxeHBrPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI2MDFjZjkzYy1kMDkxLTcwOWItODEyYS05M2Y1ZjYxNWU0ODEiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LW5vcnRoLTEuYW1hem9uYXdzLmNvbVwvZXUtbm9ydGgtMV90Q0ZuZEJjamwiLCJjb2duaXRvOnVzZXJuYW1lIjoicGlyYXNtYXR0aWEyMjk5LWF0LWdtYWlsLmNvbSIsIm9yaWdpbl9qdGkiOiI5OGUzYWI4Yy01NGQxLTRkNTEtYjM3OC1lZDAzOTAwMGMxMTYiLCJhdWQiOiIzZ29zNW4wcGUyMXVxZTdtcXVuOGJ0djFkYiIsImV2ZW50X2lkIjoiMjcwYjMyZjktODkwMC00NGY3LWI4YWQtY2VjYjZjY2MzNzU1IiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE3NTI2NzE1NTMsImV4cCI6MTc1MjY3NTE1MywiaWF0IjoxNzUyNjcxNTUzLCJqdGkiOiI0NDRmNmUyYS03ZDEwLTRmNWQtOWIxMC1hNzcwZmE4NTI5NGYiLCJlbWFpbCI6InBpcmFzbWF0dGlhMjI5OUBnbWFpbC5jb20ifQ.ODTqjEkBhvKJwjEhrihF3Ptywk3UpigDLn28Y9ny2n52rotXUkOPcsQ3gIMRGDy1ZSpx-un7aJFIcLHNbZAkF_5c0W43O_FkA4hvfsImCyNURvjz7C4BIxxmH71z45P1k-uJChg2fLOjiJySQacneHgyuKftPZC3l8CfDCzzo7W7C8sFJJbT2pCbjDPUGFaLWVQsy-Cohv8jY9p-tZxMyEGbgmffXDJtbEyWYMplvlzrmUsEu6tG8la2S7Mf710ulD8eQpGcgHl70AUk1uJm1LyC7y4LQJO7fSN40swubjhLVtUyn6hhKN6PolBgwN40VfEm1KDyMPClw2j_fUPlKQ
-*/
+
